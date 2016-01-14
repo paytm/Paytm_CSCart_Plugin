99a601c223a9502ded1f7f83312bad075e9fc36d
Paytm-Payments <gitadmin@paytm.com>
2016-01-14 17:39:19 +0530

    CS-Cart4.0 Plugin added

---
+++ paytm cs-cart 4.0/app/payments/paytm.php
@@ -0,0 +1,153 @@
+<?php
+//
+// Paytm_v2.1 - CSCart
+//
+ini_set('display_errors','On');
+error_reporting(E_ALL);
+if ( !defined('AREA') ) { die('Access denied'); }
+use Tygh\Registry;
+
+include_once('encdec_paytm.php');
+
+if (!defined('BOOTSTRAP')) { die('Access denied'); }
+// Handling response from paytm 
+
+if (defined('PAYMENT_NOTIFICATION')) {
+
+	
+	$order_id			= $_POST['ORDERID'];
+	$res_code			= $_POST['RESPCODE'];
+	$res_desc			= $_POST['RESPMSG'];
+	$checksum_recv		= $_POST['CHECKSUMHASH'];
+	$paramList			= $_POST;
+
+	if (fn_check_payment_script('paytm.php', $_POST['ORDERID'], $processor_data)){
+	
+		if (empty($processor_data)) {
+				$processor_data = fn_get_processor_data($order_info['email']);
+				}
+	$secret_key = $processor_data["processor_params"]['secret_key'];
+
+	$bool = "FALSE";
+	$bool = verifychecksum_e($paramList, $secret_key, $checksum_recv);
+
+	if (!empty($_POST['ORDERID'])) {		
+		if (fn_check_payment_script('paytm.php', $_POST['ORDERID'], $processor_data)) {		
+			$pp_response = array();			
+			$order_info = fn_get_order_info($_POST['ORDERID']);			
+			if($bool =="TRUE"){
+				if($_REQUEST['RESPCODE'] == 01){
+					$pp_response['order_status'] = 'P';
+					$pp_response['reason_text'] = "Thank you. Your order has been processed successfully. ";
+				}
+				else{
+					$pp_response['order_status'] = 'F';
+					$pp_response['reason_text'] = "Thank you. Your order has been unsuccessfull";
+				}
+			}
+			else {
+				$pp_response['order_status'] = 'D';
+				$pp_response['reason_text'] = "Thank you. Your order has been declined due to security reasons.";
+			}
+			
+			fn_change_order_status($order_id,$pp_response['order_status']);
+      fn_finish_payment($order_id, $pp_response,array());
+      fn_order_placement_routines('route',$order_id);
+      
+		}
+		exit;
+	}
+} 
+}else {
+	
+	$merchant_id = $processor_data["processor_params"]['merchant_id'];
+	$industry_type = $processor_data["processor_params"]['industry_type'];
+	$website_name = $processor_data["processor_params"]['website_name'];
+	$channel_id = $processor_data["processor_params"]['channel_id'];
+	$current_location = Registry::get('config.current_location');
+	
+	$mod = $processor_data["processor_params"]['transaction_mode'];
+	
+	$log = $processor_data['processor_params']['log_params'];
+	
+	if($mod == "test"){
+	$paytm_url =  "https://pguat.paytm.com/oltp-web/processTransaction"; 
+	}else {
+		$paytm_url = "https://secure.paytm.in/oltp-web/processTransaction";	
+	}
+	//Order Total
+	$paytm_total = fn_format_price($order_info['total']) ;
+	$amount = $paytm_total ;							// Should be in Rupees 
+	$paytm_shipping = fn_order_shipping_cost($order_info);//var_dump($order_info);exit;
+	$paytm_order_id = (($order_info['repaid']) ? ($order_id .'_'. $order_info['repaid']) : $order_id);
+	$date = date('Y-m-d H:i:s');
+	
+	$msg = fn_get_lang_var('text_cc_processor_connection');
+	$msg = str_replace('[processor]', 'paytm', $msg);
+	
+	if (!empty($order_info['items'])) {
+		foreach ($order_info['items'] as $k => $v) {
+			$v['product'] = htmlspecialchars($v['product']);
+
+		}
+	}
+	if($mod == "test")
+	$mode = 0;
+	else 
+	$mode = 1;
+	
+	$return_url =fn_url("payment_notification.notify?payment=paytm&order_id=$order_id", AREA, 'http') . '&';
+	
+	$post_variables = Array(
+            "MID" =>  $merchant_id,
+            "ORDER_ID" => $paytm_order_id,
+            "CUST_ID" => $order_info['email'],
+            "TXN_AMOUNT" =>  $amount,
+            "CHANNEL_ID" => $channel_id,
+            "INDUSTRY_TYPE_ID" => $industry_type,
+            "CALLBACK_URL" => $return_url,
+	      		"WEBSITE" => $website_name
+            );
+	
+	$secret_key = $processor_data['processor_params']['secret_key'];
+	
+		
+	if($log == "yes")
+	{
+		error_log("All Params(Parameters which are posting to paytm) : " .$all);
+		error_log("paytm Secret Key : " .$secret_key);
+	}
+
+	//$checksum = $sum->calculateChecksum($secret_key,$all);
+	$checksum = getChecksumFromArray($post_variables, $secret_key);//
+	echo <<<EOT
+	<html>
+	<body onLoad="document.paytm_form.submit();">
+	<form action="{$paytm_url}" method="post" name="paytm_form">
+	
+	<input type=hidden name="MID" value="{$merchant_id}">
+	<input type=hidden name="ORDER_ID" value="$paytm_order_id">
+	<input type=hidden name="WEBSITE" value="{$website_name}">
+	<input type=hidden name="INDUSTRY_TYPE_ID" value="{$industry_type}">
+	<input type=hidden name="CHANNEL_ID" value="{$channel_id}">
+	<input type=hidden name="TXN_AMOUNT" value="{$amount}">
+	<input type=hidden name="CUST_ID"  value="{$order_info['email']}">
+	<input type=hidden name="txnDate" type="hidden" value="{$date}" >
+    <input type=hidden name="CALLBACK_URL" value="{$return_url}"> 
+	<input type=hidden name="CHECKSUMHASH" value="{$checksum}">
+	
+	
+EOT;
+
+
+	echo <<<EOT
+	</form>
+	<div align=center>{$msg}</div>
+	</body>
+	</html>
+EOT;
+
+	fn_flush();
+}
+exit;
+?>
---
+++ paytm cs-cart 4.0/design/backend/templates/views/payments/components/cc_processors/paytm.tpl
@@ -0,0 +1,41 @@
+<div class="form-field">
+	<label for="merchant_i">{__("paytm_merchant_id")}:</label>
+	<input type="text" name="payment_data[processor_params][merchant_id]" id="merchant_id" value="{$processor_params.merchant_id}" class="input-text" />
+</div>
+
+<div class="form-field">
+	<label for="secret_key">{__("paytm_secret_key")}:</label>
+	<input type="text" name="payment_data[processor_params][secret_key]" id="secret_key" value="{$processor_params.secret_key}" class="input-text" />
+</div>
+
+<div class="form-field">
+	<label for="industry_type">{__("paytm_industry_type")}:</label>
+	<input type="text" name="payment_data[processor_params][industry_type]" id="industry_type" value="{$processor_params.industry_type}" class="input-text" />
+</div>
+
+<div class="form-field">
+	<label for="website_name">{__("paytm_website_name")}:</label>
+	<input type="text" name="payment_data[processor_params][website_name]" id="website_name" value="{$processor_params.website_name}" class="input-text" />
+</div>
+
+<div class="form-field">
+	<label for="channel_id">{__("paytm_channel_id")}:</label>
+	<input type="text" name="payment_data[processor_params][channel_id]" id="channel_id" value="{$processor_params.channel_id}" class="input-text" />
+</div>
+
+
+<div class="form-field">
+	<label for="transaction_mode">{__("paytm_transaction_mode")}:</label>
+	<select name="payment_data[processor_params][transaction_mode]" id="transaction_mode">
+		<option value="test" {if $processor_params.transaction_mode == "test"}selected="selected"{/if}>{__("paytm_test")}</option>
+		<option value="live" {if $processor_params.transaction_mode == "live"}selected="selected"{/if}>{__("paytm_live")}</option>
+	</select>
+</div>
+<div class="form-field">
+	<label for="log_params">{__("paytm_log_params")}:</label>
+	<select name="payment_data[processor_params][log_params]" id="log_params">
+		<option value="yes" {if $processor_params.log_params == "yes"}selected="selected"{/if}>{__("paytm_yes")}</option>
+		<option value="no" {if $processor_params.log_params == "no"}selected="selected"{/if}>{__("paytm_no")}</option>
+	</select>
+</div>
+
